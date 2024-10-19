6d8fe80236f44fc01337f0226aeb4169daa84a97
Kirisi15 <kkirisigan@gmail.com>
2024-10-20 00:39:35 +0530

    Created add match form ,add button is not working

--- organizer/addMatch.php
+++ organizer/addMatch.php
@@ -6,6 +6,39 @@
     <title>matchEdit</title>
 </head>
 <body>
+    <label>Team 1:</label>
+    <select name="team1">
+        <?php
+            include 'dbConnect.php';
+            $sql="SELECT * FROM team where paymentStatus=1";
+            $result=mysqli_query($conn,$sql);
+            while($row=mysqli_fetch_assoc($result))
+            {
+                echo "<option>".$row['teamName']."</option>";
+            }
+        
+    echo "</select><label>Team 2:</label>
+            <select name='team2'>";
+            include 'dbConnect.php';
+            $sql="SELECT * FROM team where paymentStatus=1";
+            $result=mysqli_query($conn,$sql);
+            while($row=mysqli_fetch_assoc($result))
+            {
+                echo "<option>".$row['teamName']."</option>";
+            }
+            echo "</select>
+            <label for='venue'>Venue</label>
+            <input type='text' name='venue'>
+            <label for='date'>Date</label>
+            <input type='date' name='date'>
+            <form method='post'><button type='submit' name='add'>Add</button></form>";
+
+            if(isset($_POST['add'])){
+                
+                
+            }
+
+        ?>
     
     
 </body>
