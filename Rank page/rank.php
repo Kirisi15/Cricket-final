eb5509f017460fd59d911918eeb6138332b2e076
Kirisi15 <kkirisigan@gmail.com>
2024-10-22 01:12:11 +0530

    Get team name and points from database need to rank

--- Rank page/rank.php
+++ Rank page/rank.php
@@ -10,14 +10,25 @@
 <?php
 include 'dbConnect.php';
 
+
 $sql="SELECT * FROM team WHERE paymentStatus=1";
 $result=mysqli_query($conn,$sql);
 if(mysqli_num_rows($result))
 {
+   
     while($row=mysqli_fetch_assoc($result))
     {
-        
+        $count=0;
+        $teamUsername=$row['teamUsername'];
+        $sql1="SELECT * FROM matches WHERE winningTeam='$teamUsername'";
+        $result1=mysqli_query($conn,$sql1);
+        while($row1=mysqli_fetch_assoc($result1))
+        {
+            $count+=1;
+        }
+        echo $teamUsername."-".$count."<br>";
     }
+   
 }
 ?>
 </body>
