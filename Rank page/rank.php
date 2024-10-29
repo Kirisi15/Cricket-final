f486daa4b86403e3798886b465b8141b8d01fe71
Kirisi15 <kkirisigan@gmail.com>
2024-10-29 19:26:38 +0530

    Add files via upload

---
+++ Rank page/rank.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+include 'dbConnect.php';
+
+$teamPoints=array();
+$sortedTeamPoints=array();
+$sql="SELECT * FROM team WHERE paymentStatus=1";
+$result=mysqli_query($conn,$sql);
+if(mysqli_num_rows($result))
+{
+   
+    while($row=mysqli_fetch_assoc($result))
+    {
+        $count=0;
+        $teamUsername=$row['teamUsername'];
+        $sql1="SELECT * FROM matches WHERE winningTeam='$teamUsername'";
+        $result1=mysqli_query($conn,$sql1);
+        while($row1=mysqli_fetch_assoc($result1))
+        {
+            $count+=1;
+        }
+        array_push($teamPoints,"$count=>$teamUsername");
+
+        
+       // echo $teamUsername."-".$count."<br>";
+    }
+    asort($teamPoints);
+    
+    rsort($teamPoints);
+    
+    echo implode("<br>", $teamPoints);
+    echo $teamPoints[0];
+    
+   
+}
+?>
+</body>
+</html>
---
+++ organizer/addMatch.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>matchEdit</title>
+</head>
+<body>
+    <form method='post'>
+        <label>Team 1:</label>
+        <select name="team1">
+            <?php
+                include 'dbConnect.php';
+                $sql="SELECT * FROM team where paymentStatus=1";
+                $result=mysqli_query($conn,$sql);
+                while($row=mysqli_fetch_assoc($result))
+                {
+                    echo "<option>".$row['teamName']."</option>";
+                }
+            
+            echo "</select><label>Team 2:</label>
+                <select name='team2'>";
+                include 'dbConnect.php';
+                $sql="SELECT * FROM team where paymentStatus=1";
+                $result=mysqli_query($conn,$sql);
+                while($row=mysqli_fetch_assoc($result))
+                {
+                    echo "<option>".$row['teamName']."</option>";
+                }
+                echo "</select>
+                <label for='venue'>Venue</label>
+                <input type='text' name='venue'>
+                <label for='date'>Date</label>
+                <input type='date' name='date'>
+                <button type='submit' name='add'>Add</button></form>";
+
+                $team1=$_POST['team1'];
+                $team2=$_POST['team2'];
+                $venue=$_POST['venue'];
+                $date=$_POST['date'];
+
+                if(isset($_POST['add'])){
+                    $sql="INSERT INTO matches(organizerId,teamIdA,teamIdB,venue,date)
+                    VALUE('10','$team1','$team2','$venue','$date')";
+
+                    mysqli_query($conn,$sql);
+                    
+                }
+
+            ?>
+    
+    
+</body>
+</html>
