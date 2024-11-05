64d09346fdcdf90b41042abb8429f5f6435afae8
Kirisi15 <kkirisigan@gmail.com>
2024-11-05 10:33:15 +0530

    Added styling to match edit page

--- admin/matchEdit.php
+++ admin/matchEdit.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>matchEdit</title>
+    <link rel="stylesheet"href="matchEdit.css">
 </head>
 <body>
     <?php
