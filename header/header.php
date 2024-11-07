dfa3c0a238629ab59b07245fbc547d185857c23b
Mihisara29 <146804234+Mihisara29@users.noreply.github.com>
2024-11-07 23:15:47 +0530

    Created an new header. Created an new footer. Re-disigned Team page. Instead of many player pages,created one player page.
    How to designed more each one.

--- header/header.php
+++ header/header.php
@@ -1,95 +1,85 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+<?php  echo "
+    <header>
 
-  <link rel='stylesheet' href='header-styles.css'>
+      <div class='left-part'>
 
-</head>
+        <div class='lpl-logo'>
+          <img src='/Cricket-final/LPL-LOGO/LPL_LOGO.png' height=130px width=130px class='LPL-LOGO'>
+        </div>
 
-<body>
+        <div class='LPL025'>
 
-<body>
-<header>
+          <div class='LPL2025'>
+            <p>LANKA<br>PREMIER<br>LEAGUE<br><span class='year'>2025</span></p>
+          </div>
 
-<div class='logos'>
+        </div>
 
-<div class='lpl-logo'>
-  <img src='/Cricket-final/LPL-LOGO/LPL_LOGO.png' height=200px width=200px>
-</div>  
-
-<div class='team-logos-and-LPL025'>
- 
-<div class='team-logos'>
-
-<div class='team-logo'>
-  <img src='/Cricket-final/Team-Logos/colombo.webp'height=80px width=80px>
-</div>
-
-<div class='team-logo'>
-  <img src='/Cricket-final/Team-Logos/dambulla.webp'height=80px width=80px>
-</div>
-
-<div class='team-logo'>
-  <img src='/Cricket-final/Team-Logos/galle.png'height=80px width=80px>
-</div>
- 
-<div class='team-logo'>
-  <img src='/Cricket-final/Team-Logos/jaffna.webp'height=80px width=80px>
-</div>
-
-<div class='team-logo'>
- <img src='/Cricket-final/Team-Logos/kandy.png'height=80px width=80px>
-</div>
-</div>
-
-<div class='LPL2025'> 
- <p>LANKA PREMIER LEAGUE <span class='year'>2025</span></p> 
- </div>
-
-</div>
+      </div>
 
 
+      <div class='middle-part'>
 
+      <div class='middle-image'>
+      <img src='http://localhost/Cricket-final/image-header/image-header.jpg' height=150 width=100%>
+      </div>
+        
+        <ul class='nav-bar'>
+          <li class='nav-links'>
+            <a href='#'>Matches</a>
+          </li>
 
-</div>
+          <li class='nav-links'>
+            <a href='#'>News</a>
+          </li>
 
- 
+          <li class='nav-links'>
+            <a href='#'>Ranking</a>
 
+          </li>
+          <li class='nav-links'>
+            <a href='#'>Team</a>
 
+          </li>
+          <li class='nav-links'>
+            <a href='#'>Register Team</a>
 
-</div>
+          </li>
+          <li class='nav-links'>
+            <a href='#'>Login</a>
+          </li>
+        </ul>
+
+      </div>
 
 
 
 
 
-</div>
-
-<div class='right-part'>
-
-<a href='https://srilankacricket.lk/'><img class='slc-logo' src='/Cricket-final/SLC-LOGO/SLC_LOGO.png' height=50px width=50px></a>
-
-<span class="followUs">follow Us</span>
-
-<div class="social-media">
-
-<a href='https://x.com/LPLT20/status/1602578678139195393'><img class="x" src='/Cricket-final/social-media/x.webp' width=48></a>
-
-<a href='https://www.instagram.com/lplt20/reel/C8elNRZhvDT/'><img class="insta" src='/Cricket-final/social-media/instagram.png' width=50></a>
-
-<a href='https://web.facebook.com/LPLT20/'><img class="fb" src='/Cricket-final/social-media/fb.webp' height=38></a>
-</div>
+      <div class='right-part'>
 
 
+      
+        <a href='https://srilankacricket.lk/'><img class='slc-logo' src='/Cricket-final/SLC-LOGO/SLC_LOGO.png' height=50px width=50px></a>
+
+        <span class='followUs'>follow Us</span>
+
+        <div class='social-media'>
+
+          <a href='https://x.com/LPLT20/status/1602578678139195393'><img class= 'x' src='/Cricket-final/social-media/x.webp' width=48></a>
+
+          <a href='https://www.instagram.com/lplt20/reel/C8elNRZhvDT/'><img class='insta' src='/Cricket-final/social-media/instagram.png' width=50></a>
+
+          <a href='https://web.facebook.com/LPLT20/'><img class='fb' src='/Cricket-final/social-media/fb.webp' height=38></a>
+        </div>
 
 
-</div>
 
 
-</header>
-  
-</body>
-</html>
+      </div>
+
+
+    </header>
+
+ "; 
+ ?>
--- team/teams.php
+++ team/teams.php
@@ -1,133 +1,412 @@
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+
+  <style>
+    .main-image-container {
+      margin-top: 50px;
+      position: relative;
+      margin-right: 40px;
+      margin-left: 40px;
+
+    }
+
+
+
+    .main-image {
+
+
+      max-height: 700px;
+      width: 100%;
+      height: 100%;
+      transition: opacity 0.5s ease;
+      animation: slideIn 1s forwards;
+      border-radius: 10px;
+
+
+    }
+
+    .image-description {
+
+      background: linear-gradient(to right, rgb(0, 96, 206, 0.2), rgb(31, 78, 156, 0.2));
+      position: absolute;
+      justify-content: center;
+      width: 500px;
+      animation: slideIn 1s forwards;
+      border-radius: 10px;
+      padding: 10px;
+      top: 30%;
+      font-family: Arial, Helvetica, sans-serif;
+      color: whitesmoke;
+      margin-top: -20px;
+
+
+    }
+
+    .image-description h1 {
+      font-weight: bolder;
+    }
+
+
+    .team-description {
+
+
+      color: whitesmoke;
+      font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+      padding: 10px;
+      border-radius: 15px;
+      max-width: 400px;
+
+    }
+
+    .team-list {
+      list-style: none;
+      display: flex;
+     
+      align-items: center;
+      gap:20px;
+
+    }
+
+    .team-logo-and-description{
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+    }
+
+
+ 
+
+    @keyframes slideOut {
+      from {
+        transform: translateX(0);
+        opacity: 1;
+      }
+
+      to {
+        transform: translateX(100%);
+        opacity: 0;
+      }
+    }
+
+
+
+    @keyframes slideIn {
+      from {
+        transform: translateX(-100%);
+        opacity: 0;
+      }
+
+      to {
+        transform: translateX(0);
+        opacity: 1;
+      }
+    }
+  </style>
+
+
 </head>
-<link rel="stylesheet" href="teams-style.css">
+
+<link rel="stylesheet" href="../header/header-styles.css">
+<link rel="stylesheet" href="teams-styles.css">
+<link rel="stylesheet" href="../footer/footer-styles.css">
 
 <body>
 
-  <header>
+  <?php
 
-  <div class='logo'>
-    <img src='/Cricket-final/LPL-LOGO/LPL_LOGO.jpg' height=80px width=80px>
- 
-    <div class='logos-LPL25'>
-   
-    <img class='logos' src='/Cricket-final/LPL-LOGO/logos.png' height=50px width=260px>
-     <ul class="topic"><li>LANKA</li>  <li>PREMIER</li> <li>LEAGUE</li> <li style="color:white;">2025</li></ul>
- 
+  include "dbConnect.php";
+
+  include "../header/header.php";
+  ?>
+  <div class='main-image-container'>
+
+
+    <img src='http://localhost/Cricket-final/team-main-images/f9be4234-9a4b-43b4-870c-fec9e23d8c57.jpg' class='main-image'>
+    <div class='image-description'>
+
+      <div class='description-main-content'>
+        <h1>Binura Fernando's Five-Wicket Haul Guides Colombo Strikers to Victory Over Galle Marvels</h1>
+        <br>
+        <p>Binura Fernando delivered a sensational performance, taking 5 wickets to dismantle the Galle Marvels' batting lineup. His outstanding spell led the Colombo Strikers to a commanding win, securing them an impressive victory in the latest Lanka Premier League clash.</p>
+      </div>
+
+
     </div>
- 
+
   </div>
 
-<div class="right-part">
-
-<span class="followUs">follow Us</span>
-
-<div class="social-media">
-
-<a href='https://x.com/LPLT20/status/1602578678139195393'><img class="x" src='/Cricket-final/social-media/x.png'></a>
-
-<a href='https://www.instagram.com/lplt20/reel/C8elNRZhvDT/'><img class="insta" src='/Cricket-final/social-media/insta.png'></a>
-
-<a href='https://web.facebook.com/LPLT20/'><img class="fb" src='/Cricket-final/social-media/fb.png' height=37></a>
-</div>
-
-<a href='https://srilankacricket.lk/'><img class='slc-logo' src='/Cricket-final/SLC-LOGO/SLC_LOGO.png' height=50px width=50px></a>
 
 
-</div>
+  <?php
+  $sql = "SELECT * FROM team";
+
+  $result = mysqli_query($conn, $sql);
+
+  if (mysqli_num_rows($result) > 0) {
+
+    echo "<ul class='team-list'>";
 
 
-  </header>
-  
-<?php
+    while ($row = mysqli_fetch_assoc($result)) {
 
-include "dbConnect.php";
 
-$sql = "SELECT * FROM team";
 
-$result = mysqli_query($conn,$sql);
+      if ($row['teamName'] === 'Colombo Strikers') {
 
-if(mysqli_num_rows($result)>0){
+        
 
-  echo"<ul class='logo-list'>";
+        echo "<li>";
+        echo "<div class='team-logo-and-description'>";
+        echo "<div class='logo'>";
+        echo "<a href='/Cricket-final/players/players.php?teamName=" . $row['teamName'] . "'>";
+        echo "<img src='/" . $row['teamLogo'] . "' width=200 height=200>";
+        echo "</a>";
+        echo "</div>
+          
+                  <div class='team-description' style='background: linear-gradient(to right,rgb(46, 0, 78,0.7), rgb(255, 0, 255,0.5));'>
+                  A powerhouse team from Sri Lanka's capital, Colombo Strikers are known for their aggressive gameplay and strong batting lineup. They have a rich history in the Lanka Premier League (LPL) and are fan favorites for their high-energy performances.
+                  </div>
+          
+          ";
 
-  while ($row = mysqli_fetch_assoc($result)) {
-echo"<li>";   
-echo "<div class='logo'>";
-echo "<a href='/Cricket-final/players/".$row['teamUsername'].".php'>";
-echo "<img src='/".$row['teamLogo']."' width=200 height=200>
-      </a>";
-echo "</div>";
-echo"</li>";
+        echo "</div>";
 
+
+
+        echo "<video width='600' class='team-video' controls>
+                    <source src='The roar that reaches from New York To Colombo.🔥Entering strong with colombostrikers at the lplt20 🐯.mp4' type='video/mp4'>
+                  </video>
+        </li>
+                  
+                  ";
+      } else if ($row['teamName'] === 'Jaffna Kings') {
+
+
+        
+
+        echo "<li>";
+        echo "<div class='team-logo-and-description'>";
+        echo "<div class='logo'>";
+        echo "<a href='/Cricket-final/players/players.php?teamName=" . $row['teamName'] . "'>";
+        echo "<img src='/" . $row['teamLogo'] . "' width=200 height=200>";
+        echo "</a>";
+        echo "</div>
+          
+                         
+              <div class='team-description' style='background: linear-gradient(to right,rgb(0,255,127), rgb(233,192,103,0.5));' >
+
+              Jaffna Kings are a dominant force in the LPL, known for their solid team structure and exceptional performances in both batting and bowling. With a fierce determination, they are a favorite for many fans, always putting on thrilling displays.
+
+              </div>
+          
+          ";
+
+        echo "</div>";
+
+        echo "<video width='600' class='team-video' controls>
+                    <source src='Vijayakanth Viyaskanth 3 Wickets .mp4' type='video/mp4'>
+                  </video>
     
-}
-echo "</ul>";
+        </li>
+                  ";
+      } else if ($row['teamName'] === 'Dambulla Thunders') {
 
-}
+        
+
+        echo "<li>";
+        echo "<div class='team-logo-and-description'>";
+        echo "<div class='logo'>";
+        echo "<a href='/Cricket-final/players/players.php?teamName=" . $row['teamName'] . "'>";
+        echo "<img src='/" . $row['teamLogo'] . "' width=200 height=200>";
+        echo "</a>";
+        echo "</div>
+
+               
+   <div class='team-description' style='background: linear-gradient(to right,rgb(227,59,19), rgb(255,255,1,0.5));'>
+
+              Representing the heart of Sri Lanka, Dambulla Thunders are known for their strong bowling attack and solid all-rounders. With a mix of youth and experience, they are a formidable team in the LPL, always fighting hard for victory.
+
+              </div>
+
+    </div>
+
+";
+
+        echo "<video width='600' class='team-video' controls>
+                    <source src='Chamindu Wickramasinghe .mp4' type='video/mp4'>
+                  </video>
+                  
+                  </li>
+                  ";
+      } else if ($row['teamName'] === 'B-Love Kandy') {
+
+        
+
+        echo "<li>";
+        echo "<div class='team-logo-and-description'>";
+        echo "<div class='logo'>";
+        echo "<a href='/Cricket-final/players/players.php?teamName=" . $row['teamName'] . "'>";
+        echo "<img src='/" . $row['teamLogo'] . "' width=200 height=200>";
+        echo "</a>";
+        echo "</div>
+
+               
+              <div class='team-description' style='background: linear-gradient(to right,rgb(167,99,27), rgb(233,192,103,0.5));'>
+
+              B-Love Kandy is a team full of passion and flair, representing Kandy with pride. Known for their aggressive batting and strong leadership, they aim to make a mark in every LPL season, captivating fans with their exciting cricketing style.
+
+              </div>
+          </div>
+";
+
+        echo "<video width='600' class='team-video' controls>
+                    <source src='B-Love Kandy LPL 2023 .mp4' type='video/mp4'>
+                  </video>
+                  
+  
+              </li>
+                  ";
+      } else if ($row['teamName'] === 'Galle Marvels') {
+
+        
+
+        echo "<li>";
+        echo "<div class='team-logo-and-description'>";
+        echo "<div class='logo'>";
+        echo "<a href='/Cricket-final/players/players.php?teamName=" . $row['teamName'] . "'>";
+        echo "<img src='/" . $row['teamLogo'] . "' width=200 height=200>";
+        echo "</a>";
+        echo "</div>
+
+               
+              <div class='team-description' style='background: linear-gradient(to right,rgb(17,18,19), rgb(151,252,1,0.5));'>
+
+               Hailing from the coastal city of Galle, this team is known for their strategic approach and balanced squad. The Galle Marvels have a mix of experienced international players and emerging talent, making them an exciting team to watch.
 
 
-?>
 
-<footer>
+              </div>
+              </div>
+";
 
-<div class='footer-left-side'>
-<div class="slclogo">  
-<img src='/Cricket-final/SLC-LOGO/SLC_LOGO02.png' height=60 width=60>
-</div>
-<div class='contact-details'>
-<adress>
-ADDRESS:
-Sri Lanka Cricket
-No 35, Maitland Place,
-Colombo 07, Sri Lanka.</adress><br>
+        echo "<video width='600' class='team-video' controls>
+                    <source src='Galle Kollo .mp4' type='video/mp4'>
+                  </video>
+                  
+              </li>     
+                  ";
+      }
 
-<email>EMAIL: info@srilanka</email><br>
-<number>PHONE NUMBER: +94112681601-4</number><br>
-<fax>FAX: +94112697405</fax>
-</div>
-</div>
 
-<div class='src'><p>©SriLankaCricket</p></div>
+      ;
+    }
 
-<div class='sponser'>
-  <img src='/Cricket-final/SPONSERS-LOGO/itw.png' height=30 width=50></sponser>
-  <img src='/Cricket-final/SPONSERS-LOGO/suquick.png' height=30px width=50px></sponser>
-  <img src='/Cricket-final/SPONSERS-LOGO/sony.png' height=30 width=35>
-</div>
-
-</footer>
-
-<script>
-
-const images = [
-        'http://localhost/Cricket-final/background-images/b-image01.png',
-        'http://localhost/Cricket-final/background-images/b-image02.png',
-        'http://localhost/Cricket-final/background-images/b-image03.png',
-        'http://localhost/Cricket-final/background-images/b-image04.png',
-    ];
-
-  let currentIndex = 0;
-
-  function changeBackground(){
-
-    currentIndex = (currentIndex + 1) % images.length;
-    document.body.style.backgroundImage = `url('${images[currentIndex]}')`;
-
+    echo "</ul>";
   }
 
-  document.body.style.backgroundImage = `url('${images[currentIndex]}')`;
+  include "../footer/footer.php";
+  ?>
 
-  setInterval(changeBackground,5000);
 
 
- </script> 
+
+
+
+
+
+
+
+
+
+
+
+  <script>
+    const images = [
+      'http://localhost/Cricket-final/team-main-images/7db9d5f8-20cf-4cf7-9535-681f21a70929.jpg',
+      'http://localhost/Cricket-final/team-main-images/445e584b-ff38-43be-973e-46a0e7f755ff.jpg',
+      'http://localhost/Cricket-final/team-main-images/81146453-b761-489a-a089-94979392b098.png',
+      'http://localhost/Cricket-final/team-main-images/ce156bb8-bb1d-49de-b242-4b72908e574b.png',
+      'http://localhost/Cricket-final/team-main-images/f9be4234-9a4b-43b4-870c-fec9e23d8c57.jpg'
+    ];
+
+    let currentIndex = 0;
+    const mainImage = document.querySelector('.main-image');
+    const descriptionMainContent = document.querySelector('.description-main-content');
+    const imageDescription = document.querySelector('.image-description');
+
+    function changeImage() {
+      mainImage.style.animation = 'slideOut 1s forwards';
+      imageDescription.style.animation = 'slideOut 1s forwards';
+
+
+      setTimeout(() => {
+        // Update the image source to the next image
+        currentIndex = (currentIndex + 1) % images.length;
+
+        mainImage.src = images[currentIndex];
+
+        if (currentIndex === 0) {
+
+          descriptionMainContent.innerHTML = `
+      <h1>"Rajapaksa's Unbeaten 105* Powers Galle Marvels to Victory Over Jaffna Kings in LPL Thriller" Give me a small description for this."</h1>
+     <br>
+      <p>Bhanuka Rajapaksa delivered a spectacular unbeaten 105*, guiding the Galle Marvels to a thrilling victory over the Jaffna Kings in the Lanka Premier League. His powerful innings proved decisive, leading his team to a memorable win in a high-stakes match.</p>
+      `
+
+        } else if (currentIndex === 1) {
+
+          descriptionMainContent.innerHTML = `
+      <h1>"
+Dasun Shanaka Leads Dambulla Thunders to LPL Final with 6-Wicket Win Over B-Love Kandy."</h1>
+     <br>
+      <p>Dasun Shanaka led Dambulla Thunders to a 6-wicket win over B-Love Kandy in the LPL 2024 semi-final. Their confident chase secured a spot in the finals, setting the stage for an exciting championship match.</p>`
+
+        } else if (currentIndex === 2) {
+
+          descriptionMainContent.innerHTML = `
+      <h1>"Jaffna Kings Crowned Champions: Clinch LPL Trophy with 42-Run Triumph Over Dambulla Thunders"</h1>
+     <br>
+      <p>Jaffna Kings claimed the LPL 2024 title, defeating Dambulla Thunders by 42 runs in a thrilling final. With a stellar all-around performance, the Kings lifted the trophy, showcasing their dominance and capping off an unforgettable season.</p>`;
+        } else if (currentIndex === 3) {
+
+          descriptionMainContent.innerHTML = `
+      <h1>"Dinesh Chandimal to Lead B-Love Kandy in Next LPL Season"</h1>
+     <br>
+      <p>B-Love Kandy is set for a fresh start in the upcoming LPL season under the captaincy of Dinesh Chandimal. With Chandimal’s leadership and experience, the team aims to make a strong impact and chase the championship title.</p>
+    `;
+        } else {
+
+          descriptionMainContent.innerHTML = `      <h1>Binura Fernando's Five-Wicket Haul Guides Colombo Strikers to Victory Over Galle Marvels</h1>
+     <br>
+      <p>Binura Fernando delivered a sensational performance, taking 5 wickets to dismantle the Galle Marvels' batting lineup. His outstanding spell led the Colombo Strikers to a commanding win, securing them an impressive victory in the latest Lanka Premier League clash.</p>`
+
+        }
+
+
+
+
+        // Reset animation and apply slide-in effect
+        mainImage.style.animation = 'none';
+        void mainImage.offsetWidth; // Trigger reflow to reset the animation
+        mainImage.style.animation = 'slideIn 1s forwards';
+
+        imageDescription.style.animation = 'none';
+        void imageDescription.offsetWidth; // Trigger reflow to reset the animation
+        imageDescription.style.animation = 'slideIn 1s forwards';
+
+      }, 1000); // Match this with animation duration
+    }
+
+    // Change image every 3 seconds
+    setInterval(changeImage, 5000);
+  </script>
+
 
 </body>
+
 </html>
