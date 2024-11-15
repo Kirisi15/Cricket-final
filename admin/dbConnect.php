7505fcb4dbff77939026385c5b555a0a08f93824
Kirisi15 <kkirisigan@gmail.com>
2024-11-15 22:45:41 +0530

    added background image

--- admin/dbConnect.php
+++ admin/dbConnect.php
@@ -3,14 +3,9 @@
     $host="localhost";
     $username="root";
     $password="";
-<<<<<<< HEAD
-    $db="cricket";
+    $db="cricket1";
 
     $conn=mysqli_connect($host,$username,$password,$db);
    
 
-=======
-    $db="cricket1";
-    $conn=mysqli_connect($host,$username,$password,$db);
->>>>>>> 27f21e5b0118289312a058b014885e353b2acd64
 ?>
